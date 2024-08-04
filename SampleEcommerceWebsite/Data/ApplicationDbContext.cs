343c6837b30b70e521e6d91aaa1b716eb18dd2d9
VishnuERHere <vishnu.er@live.com>
2024-08-04 04:06:00 -0400

    Created project and Categories page

---
+++ SampleEcommerceWebsite/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleEcommerceWebsite.Models;
+
+namespace SampleEcommerceWebsite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Tops", DisplayOrder = 1 },
+                new Category { CategoryId = 2, Name = "Bottoms", DisplayOrder = 2 },
+                new Category { CategoryId = 3, Name = "Accessories", DisplayOrder = 3 }
+                );
+        }
+    }
+}
