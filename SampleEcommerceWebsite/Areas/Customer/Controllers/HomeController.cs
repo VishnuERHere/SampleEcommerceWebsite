938cbf8c22fd0a794744b90fcc210e1c3c83520b
VishnuERHere <vishnu.er@live.com>
2024-08-05 05:00:57 -0400

    Implemented HomePage

--- SampleEcommerceWebsite/Areas/Customer/Controllers/HomeController.cs
+++ SampleEcommerceWebsite/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SampleEcommerceWebsite.DataAccess.Repository.IRepository;
 using SampleEcommerceWebsite.Models;
 using System.Diagnostics;
 
@@ -10,14 +11,18 @@ namespace SampleEcommerceWebsite.Areas.Customer.Controllers
         
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
